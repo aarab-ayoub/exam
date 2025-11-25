708bb34d14f8ab7c05a91e0f8441faab77933f72
ayaarab <ayoubaarab571@gmail.com>
2025-11-26 00:40:57 +0100

    trip

--- Trip/2/vbc.c
+++ Trip/2/vbc.c
@@ -13,7 +13,9 @@ typedef struct node {
     struct node *r;
 }   node;
 
-node *n(**s);
+node *parse_expr(char **s);
+node *parse_term(char **s);
+node *parse_factor(char **s);
 
 node    *new_node(node n)
 {
@@ -42,11 +44,12 @@ void    unexpected(char c)
         printf("Unexpected token '%c'\n", c);
     else
         printf("Unexpected end of file\n");
+    exit(1);
 }
 
 int accept(char **s, char c)
 {
-    if (**s)
+    if (**s == c)
     {
         (*s)++;
         return (1);
@@ -61,20 +64,77 @@ int expect(char **s, char c)
     unexpected(**s);
     return (0);
 }
-
-//...
-
-node    *parse_expr(char *s)
+node *parse_expr(char **s)
 {
-    //...
-
-    if (*s) 
+    node *left = parse_term(s);
+    if(!left)
+        return NULL;
+    while (accept(s ,'+'))
     {
-        destroy_tree(ret);
-        return (NULL);
+        node *right = parse_term(s);
+        if (!right)
+        {
+            destroy_tree(left);
+            return NULL;
+        }
+        node n = {ADD , 0, left ,right};
+        left = new_node(n);
+        if (!left)
+        {
+            destroy_tree(right);
+            return NULL;   
+        }   
     }
-    return (ret);
+    return left;
 }
+
+node *parse_term(char **s)
+{
+    node *left = parse_factor(s);
+    if(!left)
+        return NULL;
+    while (accept(s ,'*'))
+    {
+        node *right = parse_factor(s);
+        if (!right)
+        {
+            destroy_tree(left);
+            return NULL;
+        }
+        node n = {MULTI , 0, left ,right};
+        left = new_node(n);
+        if (!left)
+        {
+            destroy_tree(right);
+            return NULL;   
+        }   
+    }
+    return left;
+}
+
+
+node *parse_factor(char **s)
+{
+    if (isdigit(**s))
+    {
+        node n = {VAL , **s - '0' , NULL,NULL};
+        (*s)++;
+        return(new_node(n));
+    }
+    if (accept(s , '('))
+    {
+        node *n = parse_expr(s);
+        if (!expect(s , ')'))
+        {
+            destroy_tree(n);
+            return NULL;
+        }
+        return n;
+    }
+    unexpected(**s);
+    return NULL;  
+}
+
 
 int eval_tree(node *tree)
 {
@@ -93,9 +153,16 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
         return (1);
-    node *tree = parse_expr(argv[1]);
-    if (!tree)
+    char *s = argv[1];
+    node *tree = parse_expr(&s);
+    if (!tree || *s)
+    {
+        if (*s)
+            unexpected(*s);
+        else
+            unexpected(0);
         return (1);
+    }
     printf("%d\n", eval_tree(tree));
     destroy_tree(tree);
 }
