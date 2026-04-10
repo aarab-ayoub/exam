2147a675ba8a1e0842713d3a14c1b4a47b74cfe7
ayaarab <ayoubaarab571@gmail.com>
2026-04-10 16:31:07 +0100

    ok

---
+++ 005/trippp/Level 1/vect2/rendu/main.cpp
@@ -0,0 +1,37 @@
+#include "vect2.hpp"
+#include <iostream>
+
+int main()
+{
+	vect2 v1; // default constructor
+	vect2 v2(1, 2); // parametrised constructor 
+	const vect2 v3(v2); // copy constructor
+	vect2 v4 = v2; // copy assignement 
+	std::cout << "v1: " << v1 << std::endl; // << 
+	std::cout << "v1: " << "{" << v1[0] << ", " << v1[1] << "}" << std::endl; // []
+	std::cout << "v2: " << v2 << std::endl;
+	std::cout << "v3: " << v3 << std::endl;
+	std::cout << "v4: " << v4 << std::endl;
+	std::cout << v4++ << std::endl; // ++
+	std::cout << ++v4 << std::endl; // & ++
+	std::cout << v4-- << std::endl; // --
+	std::cout << --v4 << std::endl; // & --
+	v2 += v3; // +=
+	v1 -= v2; // -=
+	v2 = v3 + v3 * 2; // +(obj) AND *(int)
+	v2 = 3 * v2; // friend * (int , obj)
+	v2 += v2 += v3;
+	v1 *= 42; // *= (int)
+	v1 = v1 - v1 + v1; // -(obj) +(obj)
+	std::cout << "v1: " << v1 << std::endl;
+	std::cout << "v2: " << v2 << std::endl;
+	std::cout << "-v2: " << -v2 << std::endl; // -
+	std::cout << "v1[1]: " << v1[1] << std::endl;
+	v1[1] = 12; // refrence []
+	std::cout << "v1[1]: " << v1[1] << std::endl;
+	std::cout << "v3[1]: " << v3[1] << std::endl;
+	std::cout << "v1 == v3: " << (v1 == v3) << std::endl; // ==
+	std::cout << "v1 == v1: " << (v1 == v1) << std::endl;
+	std::cout << "v1 != v3: " << (v1 != v3) << std::endl; // !=
+	std::cout << "v1 != v1: " << (v1 != v1) << std::endl;
+}
